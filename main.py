23c3975641afc7c51ce3c6a4829950178874ea96
Laboratorio <Laboratorio@unida.edu.py>
2023-01-25 17:43:25 -0300

    add .py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from flask import flask
+from flask import Flask
+from login import login
 
-app = flask (__name__)
+
+app = Flask(__name__)
+
+
+app.register_blueprint(login)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'vivan los gatos y la sandia'
+
+
+if __name__ == "__main__":
+
+    app.run(host = '0.0.0.0', debug = True, port = 5004)
+    app.run(debug = True)
