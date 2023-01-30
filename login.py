f7127886aa6014f8f6ca28a441ea44c5332a2812
Laboratorio <Laboratorio@unida.edu.py>
2023-01-30 15:15:02 -0300

    actualizar

--- login.py
+++ login.py
@@ -9,7 +9,29 @@ def llamarServicioSet():
     global user,passw
 
     user =request.json['user']
-    passw =request.json['passw']
+    password =request.json['password']
+    inicializarVariables(user, password)
 
-    print("Username",user)
-    print("Password",passw)
+def inicializarVariables(user,password)
+global codRes, menRes,accion
+userLocal="dcaballero"
+passLocal="unida123"
+codRes = 'SIN_ERROR'
+menRes = 'OK'
+try:
+    print("verificar login")
+    if(str(password)==str(passLocal) and str(user)==str(userLocal)):
+        print("usuario y contraseña CORRECTO")
+        accion:"succes"
+        else:
+            print("usuario o contraseña INCORRECTA")
+            accion="Usuario o contraseña INCORRECTA"
+
+    except Exception as e:
+        print("ERROR", str"e")
+        codRes='ERROR'
+        menRes='Msg'+str(e)
+
+    
+
+  
