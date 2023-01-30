a846e7cf2bca93b49dc71bc71c5ee812456c7f04
Laboratorio <Laboratorio@unida.edu.py>
2023-01-30 15:34:25 -0300

    add

--- login.py
+++ login.py
@@ -6,29 +6,34 @@ login = Blueprint ('login', __name__)
 
 @login.route('/login', methods=['POST'])
 def llamarServicioSet():
-    global user,passw
+    global user,password
 
     user =request.json['user']
     password =request.json['password']
     inicializarVariables(user,password)
 
-    def inicializarVariables():
-        global codRes, menRes,accion
-        userLocal="dcaballero"
-        passLocal="unida123"
-        codRes = 'SIN_ERROR'
-        menRes = 'OK'
-        try:
-            print("Verificar login")
-            if(str(password)==str(passLocal) and str(user)==(userLocal)):
-                print("Usuario y contraseña OK ")
-                accion="succes"
-            else:
-                print("usuario o contraseña incorrecta")
-                accion="usuario o conttraseña incorrecta"
 
-        except Exception as e:
-            print ("ERROR",str(e))
-            codRes='ERROR'
-            menRes='Msg'+str(e)
+    salida = {'codRes': codRes, 'menRes': menRes, 'usuario': user, 'accion':accion}
+    return jsonify(salida)
+
+
+def inicializarVariables(user,password):
+    global codRes, menRes,accion
+    userLocal="pcabrera"
+    passLocal="michi123"
+    codRes = 'SIN_ERROR'
+    menRes = 'OK'
+    try:
+        print("Verificar login")
+        if(str(password)==str(passLocal) and str(user)==(userLocal)):
+            print("Usuario y contraseña OK ")
+            accion="succes"
+        else:
+            print("usuario o contraseña incorrecta")
+            accion="usuario o conttraseña incorrecta"
+
+    except Exception as e:
+        print ("ERROR",str(e))
+        codRes='ERROR'
+        menRes='Msg'+str(e)
 
