43cc87444d2c2f9b525ffc18258a53b19bafd216
Laboratorio <Laboratorio@unida.edu.py>
2023-01-30 15:17:02 -0300

    act

--- login.py
+++ login.py
@@ -10,28 +10,25 @@ def llamarServicioSet():
 
     user =request.json['user']
     password =request.json['password']
-    inicializarVariables(user, password)
+    inicializarVariables(user,password)
 
-def inicializarVariables(user,password)
-global codRes, menRes,accion
-userLocal="dcaballero"
-passLocal="unida123"
-codRes = 'SIN_ERROR'
-menRes = 'OK'
-try:
-    print("verificar login")
-    if(str(password)==str(passLocal) and str(user)==str(userLocal)):
-        print("usuario y contraseña CORRECTO")
-        accion:"succes"
-        else:
-            print("usuario o contraseña INCORRECTA")
-            accion="Usuario o contraseña INCORRECTA"
+    def inicializarVariables():
+        global codRes, menRes,accion
+        userLocal="dcaballero"
+        passLocal="unida123"
+        codRes = 'SIN_ERROR'
+        menRes = 'OK'
+        try:
+            print("Verificar login")
+            if(str(password)==str(passLocal) and str(user)==(userLocal)):
+                print("Usuario y contraseña OK ")
+                accion="succes"
+            else:
+                print("usuario o contraseña incorrecta")
+                accion="usuario o conttraseña incorrecta"
 
-    except Exception as e:
-        print("ERROR", str"e")
-        codRes='ERROR'
-        menRes='Msg'+str(e)
+        except Exception as e:
+            print ("ERROR",str(e))
+            codRes='ERROR'
+            menRes='Msg'+str(e)
 
-    
-
-  
